83e330b28e676b5bc767f333bdf26a509dd8ea0f
emabee <meinolf.block@sap.com>
2022-01-08 13:22:53 +0100

    added test_cdcd

--- tests/test_cdcd.rs
+++ tests/test_cdcd.rs
@@ -1,82 +1,77 @@
+use std::fs::File;
 use std::io::Write;
 
 const OUTPUT_FILE: &str = "test_cdcd.txt";
+const DUMMY_FILE: &str = "test_cdcd_dummy.txt";
 
 #[test]
 fn main() {
-    let mut output_file;
-    // std::thread::sleep(Duration::from_millis(10));
-    output_file = std::fs::OpenOptions::new()
+    let mut output_file = std::fs::OpenOptions::new()
         .write(true)
         .create(true)
         .append(true)
         .open(OUTPUT_FILE)
         .unwrap();
-    writeln!(output_file, "sdlsakjdpuwqeksadlsakd").expect("first write failed");
+    writeln!(output_file, "sdlsakjdpuwqeksadlsakd 1").expect("first write failed");
 
     std::fs::remove_file(OUTPUT_FILE).expect("first delete failed");
 
-    output_file = {
-        match std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open(OUTPUT_FILE)
-        {
-            Ok(file) => file,
-            Err(e) => match e.kind() {
-                std::io::ErrorKind::PermissionDenied => {
-                    println!("Looks like we're running in a non-standard env, like github's windows environment; giving up without error...");
-                    return;
-                }
-                _ => panic!("second open failed with {:?}", e),
-            },
-        }
-    };
-
-    writeln!(output_file, "sdlsakjdpuwqeksadlsakd 1").expect("second write failed");
+    reopen_file(&mut output_file, 2);
+    writeln!(output_file, "sdlsakjdpuwqeksadlsakd 2").expect("second write failed");
 
     std::fs::remove_file(OUTPUT_FILE).expect("second delete failed");
 
-    output_file = {
-        match std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open(OUTPUT_FILE)
-        {
-            Ok(file) => file,
-            Err(e) => match e.kind() {
-                std::io::ErrorKind::PermissionDenied => {
-                    println!("Looks like we're running in a non-standard env, like github's windows environment; giving up without error...");
-                    return;
-                }
-                _ => panic!("third open failed with {:?}", e),
-            },
-        }
-    };
-
-    writeln!(output_file, "sdlsakjdpuwqeksadlsakd 2").expect("third write failed");
+    reopen_file(&mut output_file, 3);
+    writeln!(output_file, "sdlsakjdpuwqeksadlsakd 3").expect("third write failed");
 
     std::fs::remove_file(OUTPUT_FILE).expect("third delete failed");
 
-    output_file = {
-        match std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open(OUTPUT_FILE)
-        {
-            Ok(file) => file,
-            Err(e) => match e.kind() {
-                std::io::ErrorKind::PermissionDenied => {
-                    println!("Looks like we're running in a non-standard env, like github's windows environment; giving up without error...");
-                    return;
-                }
-                _ => panic!("fourth open failed with {:?}", e),
-            },
-        }
-    };
-
+    reopen_file(&mut output_file, 4);
     writeln!(output_file, "sdlsakjdpuwqeksadlsakd 3").expect("fourth write failed");
+}
+
+fn reopen_file(output_file: &mut File, i: usize) {
+    match std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(OUTPUT_FILE)
+    {
+        Ok(file) => {
+            *output_file = file;
+        }
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::PermissionDenied => {
+                *output_file = std::fs::OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .append(true)
+                    .open(DUMMY_FILE)
+                    .unwrap();
+                // std::thread::sleep(std::time::Duration::from_millis(5000));
+                std::fs::remove_file(DUMMY_FILE).expect("second delete failed");
+
+                match std::fs::OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .append(true)
+                    .open(OUTPUT_FILE)
+                {
+                    Ok(file) => {
+                        *output_file = file;
+                    }
+                    Err(_e) => {
+                        println!(
+                            "Looks like we're running in a non-standard env, \
+                             like github's fake windows, \
+                             and obviously we're not able to overcome this somewhat weird issue; \
+                            giving up without error..."
+                        );
+                        return;
+                    }
+                }
+            }
+            _ => panic!("{}. open failed with {:?}", i, e),
+        },
+    }
 }
